ae3298c64616a2f0d15a9bf0e105d2d5bed200c6
priya1701 <piyakumarib@gmail.com>
2018-10-06 11:40:48 +0530

    Added add guest

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -71,7 +71,7 @@ class App extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={dashboardRoutes}
-          logoText={"ManuFacturer"}
+          logoText={"Krishna Oberoi"}
           logo={logo}
           image={image}
           handleDrawerToggle={this.handleDrawerToggle}
--- src/views/UserProfile/AddProduct.jsx
+++ src/views/UserProfile/AddProduct.jsx
@@ -38,122 +38,46 @@ class MyForm extends Component {
 
     constructor(props) {
         super(props);
-        this.onChangeSamplePckg = this.onChangeSamplePckg.bind(this);
-        this.onChangeSGTIN = this.onChangeSGTIN.bind(this);
-        this.onChangeBatchNo = this.onChangeBatchNo.bind(this);
-        this.onChangeProductNDC = this.onChangeProductNDC.bind(this);
-        this.onChangeStartMrktng = this.onChangeStartMrktng.bind(this);
-        this.onChangeEndMrktng = this.onChangeEndMrktng.bind(this);
-        this.onChangeNDCExFlag = this.onChangeNDCExFlag.bind(this);
-        this.onChangePckgDisc = this.onChangePckgDisc.bind(this);
-        this.onChangeNDCPckg = this.onChangeNDCPckg.bind(this);
-        this.onChangeProdId = this.onChangeProdId.bind(this);
-        this.onChangeExpiry = this.onChangeExpiry.bind(this);
-        this.onChangeManufacturer = this.onChangeManufacturer.bind(this);
-        this.onChangeDrug = this.onChangeDrug.bind(this);
-        this.onChangeApplicationNo = this.onChangeApplicationNo.bind(this);
-        this.onChangeLotId = this.onChangeLotId.bind(this);
-        this.onChangeSSCC = this.onChangeSSCC.bind(this);
-
+        this.onChangeGuestId = this.onChangeGuestId.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeType = this.onChangeType.bind(this);
+        this.onChangeCheckIn = this.onChangeCheckIn.bind(this);
+        this.onChangeCheckOut = this.onChangeCheckOut.bind(this);
+        
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
-            SGTIN: '',
-            SAMPLE_PACKAGE: '',
-            BATCHNUMBER: '',
-            PRODUCTNDC: '',
-            STARTMARKETINGDATE: '',
-            NDC_EXCLUDE_FLAG: '',
-            ENDMARKETINGDATE: '',
-            PACKAGEDESCRIPTION: '',
-            NDCPACKAGECODE: '',
-            PRODUCTID: '',
-            EXPIRYDATE: '',
-            MANUFACTURER: '',
-            DRUG: '',
-            APPLICATIONNUMBER: '',
-            lotId: '',
-            SSCC: ''
+            guestId:'',
+            name:'',
+            type:'',
+            checkIn:'',
+            checkOut:'',
+            hotel:'KrishnaOberoi'
         }
     }
-    onChangeSamplePckg(e) {
+    onChangeGuestId(e) {
         this.setState({
-            SAMPLE_PACKAGE: e.target.value
+            guestId: e.target.value
         });
     }
-    onChangeSGTIN(e) {
+    onChangeName(e) {
         this.setState({
-            SGTIN: e.target.value
+            name: e.target.value
         });
     }
-    onChangeBatchNo(e) {
+    onChangeType(e) {
         this.setState({
-            BATCHNUMBER: e.target.value
+            type: e.target.value
         });
     }
-    onChangeProductNDC(e) {
+    onChangeCheckIn(e) {
         this.setState({
-            PRODUCTNDC: e.target.value
+            checkIn: e.target.value
         });
     }
-    onChangeStartMrktng(e) {
+    onChangeCheckOut(e) {
         this.setState({
-            STARTMARKETINGDATE: e.target.value
-        });
-    }
-    onChangeEndMrktng(e) {
-        this.setState({
-            ENDMARKETINGDATE: e.target.value
-        });
-    }
-    onChangeNDCExFlag(e) {
-        this.setState({
-            NDC_EXCLUDE_FLAG: e.target.value
-        });
-    }
-    onChangePckgDisc(e) {
-        this.setState({
-            PACKAGEDESCRIPTION: e.target.value
-        });
-    }
-    onChangeNDCPckg(e) {
-        this.setState({
-            NDCPACKAGECODE: e.target.value
-        });
-    }
-    onChangeProdId(e) {
-        this.setState({
-            PRODUCTID: e.target.value
-        });
-    }
-    onChangeExpiry(e) {
-        this.setState({
-            EXPIRYDATE: e.target.value
-        });
-    }
-    onChangeManufacturer(e) {
-        this.setState({
-            MANUFACTURER: e.target.value
-        });
-    }
-    onChangeDrug(e) {
-        this.setState({
-            DRUG: e.target.value
-        });
-    }
-    onChangeApplicationNo(e) {
-        this.setState({
-            APPLICATIONNUMBER: e.target.value
-        });
-    }
-    onChangeLotId(e) {
-        this.setState({
-            lotId: e.target.value
-        });
-    }
-    onChangeSSCC(e) {
-        this.setState({
-            SSCC: e.target.value
+            checkOut: e.target.value
         });
     }
     
@@ -161,43 +85,22 @@ class MyForm extends Component {
     onSubmit(e) {
         e.preventDefault();
         const newData = {
-            SGTIN: this.state.SGTIN,
-            SAMPLE_PACKAGE: this.state.SAMPLE_PACKAGE,
-            BATCHNUMBER: this.state.BATCHNUMBER,
-            PRODUCTNDC: this.state.PRODUCTNDC,
-            STARTMARKETINGDATE: this.state.STARTMARKETINGDATE,
-            NDC_EXCLUDE_FLAG: this.state.NDC_EXCLUDE_FLAG,
-            ENDMARKETINGDATE: this.state.ENDMARKETINGDATE,
-            PACKAGEDESCRIPTION: this.state.PACKAGEDESCRIPTION,
-            NDCPACKAGECODE: this.state.NDCPACKAGECODE,
-            PRODUCTID: this.state.PRODUCTID,
-            EXPIRYDATE: this.state.EXPIRYDATE,
-            MANUFACTURER: this.state.MANUFACTURER,
-            DRUG: this.state.DRUG,
-            APPLICATIONNUMBER: this.state.APPLICATIONNUMBER,
-            lotId: this.state.lotId,
-            SSCC: this.state.SSCC
+            guestId: this.state.guestId,
+            name: this.state.name,
+            type: this.state.type,
+            checkIn: this.state.checkIn,
+            checkOut: this.state.checkOut,
+            hotel:this.state.hotel
         }
-        axios.post('http://13.126.150.151:3000/api/Product', newData)
+        axios.post('http://138.68.51.48:3000/api/guest', newData)
         .then(res => console.log(res.data));
-        console.log("PostData: "+this.state);
+        //console.log("PostData: "+this.state);
         this.setState({
-            SGTIN: '',
-            SAMPLE_PACKAGE: '',
-            BATCHNUMBER: '',
-            PRODUCTNDC: '',
-            STARTMARKETINGDATE: '',
-            NDC_EXCLUDE_FLAG: '',
-            ENDMARKETINGDATE: '',
-            PACKAGEDESCRIPTION: '',
-            NDCPACKAGECODE: '',
-            PRODUCTID: '',
-            EXPIRYDATE: '',
-            MANUFACTURER: '',
-            DRUG: '',
-            APPLICATIONNUMBER: '',
-            lotId: '',
-            SSCC: ''
+            guestId:'',
+            name:'',
+            type:'',
+            checkIn:'',
+            checkOut:''
         });
     }
 
@@ -208,16 +111,16 @@ class MyForm extends Component {
                         <GridItem xs={12} sm={12} md={12}>
                         <Card>
                             <CardHeader color="primary">
-                            <h4 className={classes.cardTitleWhite}>Add Drug</h4>
+                            <h4 className={classes.cardTitleWhite}>Add Details</h4>
                             </CardHeader>
                             <CardBody>
                             <GridContainer>
                                 <GridItem xs={12} sm={12} md={3}>
                                 <CustomInput
-                                    value={this.state.SGTIN}
-                                    onChange={this.onChangeSGTIN}
-                                    labelText="SGTIN"
-                                    id="SGTIN"
+                                    value={this.state.guestId}
+                                    onChange={this.onChangeGuestId}
+                                    labelText="Guest Id"
+                                    id="guestId"
                                     formControlProps={{
                                     fullWidth: true
                                     }}
@@ -225,21 +128,10 @@ class MyForm extends Component {
                                 </GridItem>
                                 <GridItem xs={12} sm={12} md={3}>
                                 <CustomInput
-                                value={this.state.SAMPLE_PACKAGE}
-                                onChange={this.onChangeSamplePckg}
-                                labelText="Sample Package"
-                                id="SAMPLE_PACKAGE"
-                                formControlProps={{
-                                fullWidth: true
-                                }}
-                            />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.BATCHNUMBER}
-                                    onChange={this.onChangeBatchNo}
-                                    labelText="Batch No"
-                                    id="BATCHNUMBER"
+                                    value={this.state.name}
+                                    onChange={this.onChangeName}
+                                    labelText="Name of Guest"
+                                    id="name"
                                     formControlProps={{
                                     fullWidth: true
                                     }}
@@ -247,10 +139,10 @@ class MyForm extends Component {
                                 </GridItem>
                                 <GridItem xs={12} sm={12} md={3}>
                                 <CustomInput
-                                    value={this.state.PRODUCTNDC}
-                                    onChange={this.onChangeProductNDC}
-                                    labelText="Product NDC"
-                                    id="PRODUCTNDC"
+                                    value={this.state.type}
+                                    onChange={this.onChangeType}
+                                    labelText="Id Proof"
+                                    id="type"
                                     formControlProps={{
                                     fullWidth: true
                                     }}
@@ -260,10 +152,10 @@ class MyForm extends Component {
                             <GridContainer>
                                 <GridItem xs={12} sm={12} md={3}>
                                 <CustomInput
-                                    value={this.state.STARTMARKETINGDATE}
-                                    onChange={this.onChangeStartMrktng}
-                                    labelText="Start Marketing Date"
-                                    id="STARTMARKETINGDATE"
+                                    value={this.state.checkIn}
+                                    onChange={this.onChangeCheckIn}
+                                    labelText="Check-In Time"
+                                    id="checkIn"
                                     formControlProps={{
                                     fullWidth: true
                                     }}
@@ -271,124 +163,10 @@ class MyForm extends Component {
                                 </GridItem>
                                 <GridItem xs={12} sm={12} md={3}>
                                 <CustomInput
-                                    value={this.state.NDC_EXCLUDE_FLAG}
-                                    onChange={this.onChangeNDCExFlag}
-                                    labelText="NDC Exclude Flag"
-                                    id="NDC_EXCLUDE_FLAG"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.ENDMARKETINGDATE}
-                                    onChange={this.onChangeEndMrktng}
-                                    labelText="End Marketing Date"
-                                    id="ENDMARKETINGDATE"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.PACKAGEDESCRIPTION}
-                                    onChange={this.onChangePckgDisc}
-                                    labelText="Package Description"
-                                    id="PACKAGEDESCRIPTION"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                            </GridContainer>
-                            <GridContainer>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.NDCPACKAGECODE}
-                                    onChange={this.onChangeNDCPckg}
-                                    labelText="NDC Package Code"
-                                    id="NDCPACKAGECODE"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.PRODUCTID}
-                                    onChange={this.onChangeProdId}
-                                    labelText="Pruduct Id"
-                                    id="PRODUCTID"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.EXPIRYDATE}
-                                    onChange={this.onChangeExpiry}
-                                    labelText="Expiry Date"
-                                    id="EXPIRYDATE"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.MANUFACTURER}
-                                    onChange={this.onChangeManufacturer}
-                                    labelText="Manufacturer"
-                                    id="MANUFACTURER"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                            </GridContainer>
-                            <GridContainer>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.DRUG}
-                                    onChange={this.onChangeDrug}
-                                    labelText="Drug"
-                                    id="DRUG"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.APPLICATIONNUMBER}
-                                    onChange={this.onChangeApplicationNo}
-                                    labelText="Application Number"
-                                    id="APPLICATIONNUMBER"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.lotId}
-                                    onChange={this.onChangeLotId}
-                                    labelText="Lot Id"
-                                    id="lotId"
-                                    formControlProps={{
-                                    fullWidth: true
-                                    }}
-                                />
-                                </GridItem>
-                                <GridItem xs={12} sm={12} md={3}>
-                                <CustomInput
-                                    value={this.state.SSCC}
-                                    onChange={this.onChangeSSCC}
-                                    labelText="SSCC"
-                                    id="SSCC"
+                                    value={this.state.checkOut}
+                                    onChange={this.onChangeCheckOut}
+                                    labelText="Check-Out Time"
+                                    id="checkOut"
                                     formControlProps={{
                                     fullWidth: true
                                     }}
